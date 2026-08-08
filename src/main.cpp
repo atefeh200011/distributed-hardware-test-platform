22aa1aeb1042324d3547e655a7e0cf087efeded9
atf200011 <atefehmohammadpour2@gmail.com>
2026-08-08 22:03:51 +0200

    feat: read command-line input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::cout << "Hardware Test Platform version 0.1.0\n";
+    std::cout << "hwtest> ";
+
+    std::string command;
+    std::getline(std::cin, command);
+
+    std::cout << "Received command: " << command << '\n';
     return 0;
 }
